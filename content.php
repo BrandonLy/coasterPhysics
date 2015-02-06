b1dc1e60031febf8f8957fe43f24091875d6840d
BrandonLy <brandon@brandonly.me>
2015-02-05 19:15:29 -0800

    Added starter content

--- content.php
+++ content.php
@@ -8,23 +8,25 @@ include("inc/header.php");
 
     <p>Here you'll find the information that we covered in our video located <a href="video.php">here</a></p>
     
-    <div class="accordion-title"><h3>Potential Energy</h3></div>
-    <div class="accordion-content">1</div>
+    <div class="accordion-title">
+        
+        <h3>Forces</h3>
+        <p>Centripetal Force describes the pull of an object towards the center of a round object. On roller coasters, the infamous loops are great examples of where we might see this in real life. The cart moves along the rails and seemingly defies gravity as it completes a full circle. One might expect the cart to either fall off or simply keep going, resisting the track's attempts to keep it on course. We can all thank this invisible force for making these thrilling paths possible.</p>
+        <strong>Click me to see more!</strong>
+
+    </div>
+    <div class="accordion-content">
+        <p>The equation for Centripetal Force is modeled by F<sub>c</sub> = mv<sup>2</sup>/r. The F represents force, the m mass, v velocity, and r the radius of the circular path. Not only does Centripetal Force have its own equation for force, there is also an equation for calculating Centripetal Acceleration, which can be modeled by a<sub>c</sub> = v<sup>2</sup>/r.This force plays a critical role in many apsects of our lives. Keeping everything orbiting properly is probably one of the more important things it does for us. Speaking of the moon, back in the 1600's Sir Isaac Newton had plenty of time to think about the moon and Earth's relationship. He deduced that some force must be acting upon it in order to prevent it from traveling away. This was the beginning work on Centripetal Force.</p>
+        <p>Now, clearly the technology exists to make insanely fast roller coaster. So why don't we? The main reason is that speed does not always create a more euphoric and enjoyable ride. Typically people derive joy from the "weightlessness" associated with riding. This feeling can be attributed to the Normal forces on a coaster varying from the normal levels. Now to understand why, it's important to understand how normal force works.</p>
+        <p>Normal Force is the force applied to an object by a surface. A very basic example involves a person standing still. The two forces acting upon them are normal and gravity. Gravity is causing the person to accelerate towards Earth, while the Earth is pushing back on the person. This force is exactly the same amount as the gravitational pull on the person. This phenomenom is Normal Force.</p>
+        <p>So on a roller coaster, the cart and tracks supply the Normal Force for the riders. As the riders reach the peak of the loop, they are upside down. Gravity also pulls straight down, nothing unusual about that. However, out of the ordinary is the direction the Normal Force is being applied. We often associated Normal Force with always facing upwards, because most of our surfaces are flat and horizontal, but Normal Force is always perpendicular to the surface. So when a person is upside down, the Normal Force and Gravity are acting in the same direction. This results in a perceived decreased weight. The inverse applies when a person has reached the bottom of the roller coaster.</p>
+    </div>
     
-    <div class="accordion-title"><h3>Kinetic Energy</h3></div>
+    <div class="accordion-title"><h3>Energy</h3></div>
     <div class="accordion-content">2</div>
     
     <div class="accordion-title"><h3>Acceleration</h3></div>
     <div class="accordion-content">3</div>
-    
-    <div class="accordion-title"><h3>Changes in Gravity</h3></div>
-    <div class="accordion-content">4</div>
-    
-    <div class="accordion-title"><h3>Centripetal Force</h3></div>
-    <div class="accordion-content">5</div>
-    
-    <div class="accordion-title"><h3>Velocity</h3></div>
-    <div class="accordion-content">6</div>
     
 
 <h1>Additional Helpful Content</h1>
