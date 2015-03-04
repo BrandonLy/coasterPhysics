2a8e000e256e082630be3cc8e5a5d81034c88b54
Brandon Ly <brandon@brandonly.me>
2015-03-04 09:56:56 -0800

    fixed pull request

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
 <?php $pageTitle="Home" ; $section="index" ; include( "inc/header.php"); ?>
 
-you suck
+<div id="home-tabs">
+
+    <div id="tabs-wrapper">
+        <a href="video.php">
+            <div id="tab-1" class="homeTab">
+                <div class="tabcontentWrapper verticalAlign">
+                    <img src="assets/img/picto_philo.png" width="100%" class="tab-image">
+                    <h3 class="tabTitle">coaster footage</h3>
+                    <p class="tabBlurb">See the footage behind how roller coasters work and experience a coaster with the team!</p>
+                </div>
+            </div>
+        </a>
+        <a href="animation.php">
+            <div id="tab-2" class="homeTab">
+                <div class="tabcontentWrapper verticalAlign">
+                    <img src="assets/img/picto_philo.png" width="100%" class="tab-image">
+                    <h3 class="tabTitle">our animation</h3>
+                    <p class="tabBlurb">Take a look at how our animators recreated the coaster and how the physics played their part!</p>
+                </div>
+            </div>
+        </a>
+        <a href="content.php">
+            <div id="tab-3" class="homeTab">
+                <div class="tabcontentWrapper verticalAlign">
+                    <img src="assets/img/picto_philo.png" width="100%" class="tab-image">
+                    <h3 class="tabTitle">physics and more</h3>
+                    <p class="tabBlurb">Learn more in depth about how roller coasters actually work, from centripedal force to energy!</p>
+                </div>
+            </div>
+        </a>
+    </div>
+
+</div>
+
+<?php include( "inc/footer.php");?>
