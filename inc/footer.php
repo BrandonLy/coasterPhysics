cc1f25a430b1f5ec639107b093a916c966e76e3f
Brandon Ly <brandon@brandonly.me>
2015-03-11 21:23:26 -0700

    test contact in footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,5 @@
 <footer>
+    <div class="left-align verticalAlign"><a href="contact.php">Contact Us!</a></div>
     <p class="verticalAlign">&copy; <?php echo date('Y'); ?></p>
     <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
     <script src="assets/js/main.js"></script>
