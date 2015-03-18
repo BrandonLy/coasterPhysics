7e94b8aba57eec40e21ef59b2a3bd54acd7cc834
Jonathan Walton <Jonathan_iMac@iMac.local>
2015-03-17 23:02:38 -0700

    asdfsdfsdf

--- video.php
+++ video.php
@@ -4,7 +4,7 @@
     <div class="content-wrapper">
         <center>
             <iframe class="video" src="https://www.youtube.com/embed/LKioumCtBxM" frameborder="0" allowfullscreen></iframe>
-            <p class="focusParagraph">Feel like checking out a more professional shot of the coaster? Try checking out this video by <a href="http://youtu.be/cHbIzVNe3_o">Coaster Force</a>
+            <p class="focusParagraph top-padding">Feel like checking out a more professional shot of the coaster? Try checking out this video by <a href="http://youtu.be/cHbIzVNe3_o">Coaster Force</a>
             </p>
         </center>
     </div>
